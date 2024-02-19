462007e223aa8b1567d713c12b6a41a910997b91
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2024-02-19 11:29:28 -0300

    Update dataBase.sql

--- DOCS/dataBase.sql
+++ DOCS/dataBase.sql
@@ -1,44 +1,57 @@
 CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
-  "username" VARCHAR(255) NOT NULL,
-  "email" VARCHAR(255) UNIQUE NOT NULL,
-  "password_hash" VARCHAR(255) NOT NULL,
-  "admin" boolean DEFAULT false,
-  "created_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
+  "username" VARCHAR(100) NOT NULL,
+  "company_name" VARCHAR(100) NOT NULL,
+  "email" VARCHAR(100) UNIQUE NOT NULL,
+  "password_hash" VARCHAR(300) NOT NULL,
+  "admin" BOOLEAN DEFAULT false,
+  "last_access" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT NULL,
+  "deleted_at" TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE "services" (
+  "id" SERIAL PRIMARY KEY,
+  "service_name" VARCHAR(100) NOT NULL,
+  "description" VARCHAR(100) NOT NULL,
+  "price" DECIMAL(10,2) NOT NULL,
+  "user_id" INT REFERENCES "users" ("id") ON DELETE SET NULL,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT NULL,
+  "deleted_at" TIMESTAMP DEFAULT NULL
 );
 
 CREATE TABLE "clients" (
   "id" SERIAL PRIMARY KEY,
-  "name" VARCHAR(255) NOT NULL,
-  "email" VARCHAR(255) UNIQUE,
-  "phone" VARCHAR(50),
-  "user_id" INT,
-  "plano_id" INT,
-  "created_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
+  "name" VARCHAR(100) NOT NULL,
+  "email" VARCHAR(100) UNIQUE,
+  "phone" VARCHAR(50) NOT NULl,
+  "user_id" INT REFERENCES "users" ("id") ON DELETE SET NULL,
+  "plano_id" INT REFERENCES "plan" ("id") ON DELETE SET NULL,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT NULL,
+  "deleted_at" TIMESTAMP DEFAULT NULL
 );
 
 CREATE TABLE "schedules" (
   "id" SERIAL PRIMARY KEY,
-  "user_id" INT,
-  "client_id" INT,
   "scheduled_date" TIMESTAMP NOT NULL,
-  "notes" TEXT,
-  "created_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
+  "description" VARCHAR(100) NOT NULL,
+  "user_id" INT REFERENCES "users" ("id") ON DELETE SET NULL,
+  "client_id" INT REFERENCES "clients" ("id") ON DELETE SET NULL,
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT NULL,
+  "deleted_at" TIMESTAMP DEFAULT NULL
 );
 
-CREATE TABLE "plano" (
+CREATE TABLE "plan" (
   "id" SERIAL PRIMARY KEY,
-  "name" VARCHAR(255) NOT NULL,
-  "description" TEXT,
+  "name" VARCHAR(100) NOT NULL,
+  "description" VARCHAR(100) NOT NULL,
   "price" DECIMAL(10,2) NOT NULL,
   "duration_months" INT NOT NULL,
-  "created_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
+  "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" TIMESTAMP DEFAULT NULL,
+  "deleted_at" TIMESTAMP DEFAULT NULL
 );
-
-ALTER TABLE "clients" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
-
-ALTER TABLE "schedules" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
-
-ALTER TABLE "schedules" ADD FOREIGN KEY ("client_id") REFERENCES "clients" ("id");
-
-ALTER TABLE "clients" ADD FOREIGN KEY ("plano_id") REFERENCES "plano" ("id");
